f717a97955bca6bf2fba742d51a89c0ba5b053ac
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-31 17:55:46 -0500

    added int array

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -9,6 +9,7 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create two Lists of type int.
              * Name one List "evens"
